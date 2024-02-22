e1a42fa374d9fe0d61ddc4d04a9bde236f558170
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2024-02-22 17:59:48 +0200

    Feat: added users and products model

---
+++ models/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+  discount: number;
+  description: string;
+  count: number;
+  qty: number
+  category: string;
+}
