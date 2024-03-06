ee78351bc5ad895adfc73aa63f39c70a3471c7c8
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2024-03-06 10:14:13 +0200

    CLP: removed the examlpe from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Button from "./components/button/Button";
 
 const App = () => {
-  return (
-    <div className="text-2xl text-center">
-      App
-      <Button text="Button" type="button" primary/>
-    </div>
-  );
+  return <div className="text-2xl text-center">App</div>;
 };
 
 export default App;
